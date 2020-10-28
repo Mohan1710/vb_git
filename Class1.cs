c3bb4343d09ca707ab98db1a9646f62adaf48b99
Mohan1710 <73586054+Mohan1710@users.noreply.github.com>
2020-10-28 19:00:15 +0530

    add int d

--- Class1.cs
+++ Class1.cs
@@ -5,12 +5,13 @@ public class Class1
 	public Class1()
 	{
 		
-	 static void Main(string[] args)
-		{
+	 
 			int a = 10;
 			int b = 12;
-			int c = a + b; 
-			Console.WriteLine(c);
+			int d = 22;
+			int c = a + b;
+		
+			Console.WriteLine(c+d);
 		}
 		}
 }
