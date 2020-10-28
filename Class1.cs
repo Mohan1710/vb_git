56c6c354dc741cfa82ab9f0ab2125e486db62779
Mohan1710 <73586054+Mohan1710@users.noreply.github.com>
2020-10-28 18:44:01 +0530

    add 2 number

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		
+	 static void Main(string[] args)
+		{
+			int a = 10;
+			int b = 12;
+			int c = a + b; 
+			Console.WriteLine(c);
+		}
+		}
+}
